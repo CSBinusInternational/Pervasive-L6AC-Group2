537591d4ec56b292f71ec3377f6ba36b5132ef65
adamhw <adam.halim@yahoo.com>
2017-05-09 10:07:31 +0700

    update

---
+++ final_code.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+buzzerPin = 17
+ledPin = 23
+sensorPin = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(buzzerPin, GPIO.OUT)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(sensorPin, GPIO.IN)
+
+door = 0
+
+while True:
+    if GPIO.input(sensorPin):
+        GPIO.output(ledPin, GPIO.HIGH)
+        print "OPEN"
+        time.sleep(1)
+    else:
+        GPIO.output(ledPin, GPIO.LOW)
+        print "CLOSED"
+        time.sleep(1)
