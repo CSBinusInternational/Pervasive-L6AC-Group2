adcff1052b0eff68e13449a19f3582d6c71f0a7c
claudiayhaliz@yahoo.com <claudiayhaliz@yahoo.com>
2017-05-17 08:04:40 +0700

    tested sensor and camera connection

--- Week 9/send email/sendemail.py
+++
@@ -1,9 +0,0 @@
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("diyhaliz@gmail.com", "MYPASSWORD")
- 
-msg = "Test python no 1 raspberry"
-server.sendmail("diyhaliz@gmail.com", "adam.halim@yahoo.com", msg)
-server.quit()
