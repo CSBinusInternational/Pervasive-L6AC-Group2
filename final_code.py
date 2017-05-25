d825342d7604abeaf6154f48c79468f6efc62a3f
claudiayhaliz@yahoo.com <claudiayhaliz@yahoo.com>
2017-05-25 22:03:16 +0700

    final script for system

--- final_code.py
+++ final_code.py
@@ -1,34 +1,76 @@
+# GPIO imports
 import RPi.GPIO as GPIO
 import time
 import picamera
 
+# email imports
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+# declare pin nums
 buzzerPin = 17
-ledPin = 23
 sensorPin = 27
 camera = picamera.PiCamera()
+camera.rotation = 180
 
-inputCounter = []
-
+# set pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(buzzerPin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.LOW)
+# initialize pins
+GPIO.setup(buzzerPin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(sensorPin, GPIO.IN)
 
+# loop until stop
 while True:
-        openCounter = 0
-        for c in range (0, 10):
-                time.sleep(0.05)
-                if GPIO.input(sensorPin):
-                        openCounter += 1
-
-        if openCounter > 3:
-                GPIO.output(ledPin, GPIO.HIGH)
+        if GPIO.input(sensorPin):
+                time.sleep(0.5)
+                GPIO.output(buzzerPin, GPIO.LOW)
+                camera.capture('image.jpg')
                 print "OPEN"
-                camera.capture('image1.jpg')
                 break
         else:
-                GPIO.output(ledPin, GPIO.LOW)
+                GPIO.output(buzzerPin, GPIO.HIGH)
                 print "CLOSED"
+				
+time.sleep(3)
+GPIO.output(buzzerPin, GPIO.HIGH)
 	
+GPIO.cleanup()
+
+# send email with image attached
+fromaddr = "diyhaliz@gmail.com"
+toaddr = "claudiayhaliz@yahoo.com"
+
+# create container for the message
+msg = MIMEMultipart()
+ 
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Pervasive Alarm Security System Notification"
+ 
+body = "Please see the attached image. (This is sent from Raspberry.)"
+ 
+msg.attach(MIMEText(body, 'plain'))
+
+# set image and attach
+filename = "image.jpg"
+attachment = open("/home/pi/Downloads/image.jpg", "rb")
+ 
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+msg.attach(part)
+
+# login to sender's email acc and send email
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "MYPASSWORD")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
